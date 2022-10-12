c3a057baa872393f292590a474f135b2e13d9fb9
Marius Andra <marius.andra@gmail.com>
2022-10-12 14:05:40 +0200

    docusaurus 2

--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
 const path = require("path");
 
-module.exports = function(context) {
-  const { siteConfig } = context;
-  const { themeConfig } = siteConfig;
-  const { posthog } = themeConfig || {};
-
-  if (!posthog) {
+module.exports = function (context, options) {
+  if (!options.apiKey) {
     throw new Error(
-      `You need to specify 'posthog' object in 'themeConfig' with 'apiKey' field in it to use posthog-docusaurus`
+      `You need to specify an 'apiKey' to use posthog-docusaurus`
     );
   }
 
@@ -16,7 +12,7 @@ module.exports = function(context) {
     appUrl = "https://app.posthog.com",
     enableInDevelopment = false,
     ...rest
-  } = posthog;
+  } = options;
 
   if (!apiKey) {
     throw new Error(
@@ -40,7 +36,7 @@ module.exports = function(context) {
         return {};
       }
 
-      const options = { api_host: appUrl, ...rest }
+      const posthogInitOptions = { api_host: appUrl, ...rest };
 
       return {
         headTags: [
@@ -48,20 +44,20 @@ module.exports = function(context) {
             tagName: "link",
             attributes: {
               rel: "preconnect",
-              href: appUrl
-            }
+              href: appUrl,
+            },
           },
           {
             tagName: "script",
             innerHTML: `
               !function(t,e){var o,n,p,r;e.__SV||(window.posthog=e,e._i=[],e.init=function(i,s,a){function g(t,e){var o=e.split(".");2==o.length&&(t=t[o[0]],e=o[1]),t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}}(p=t.createElement("script")).type="text/javascript",p.async=!0,p.src=s.api_host+"/static/array.js",(r=t.getElementsByTagName("script")[0]).parentNode.insertBefore(p,r);var u=e;for(void 0!==a?u=e[a]=[]:a="posthog",u.people=u.people||[],u.toString=function(t){var e="posthog";return"posthog"!==a&&(e+="."+a),t||(e+=" (stub)"),e},u.people.toString=function(){return u.toString(1)+".people (stub)"},o="capture identify alias people.set people.set_once set_config register register_once unregister opt_out_capturing has_opted_out_capturing opt_in_capturing reset".split(" "),n=0;n<o.length;n++)g(u,o[n]);e._i.push([i,s,a])},e.__SV=1)}(document,window.posthog||[]);
-              posthog.init(${JSON.stringify(
-                apiKey
-              )}, ${JSON.stringify(options)})
-            `
-          }
-        ]
+              posthog.init(${JSON.stringify(apiKey)}, ${JSON.stringify(
+              posthogInitOptions
+            )})
+            `,
+          },
+        ],
       };
-    }
+    },
   };
 };
